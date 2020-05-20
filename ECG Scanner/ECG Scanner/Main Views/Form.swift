c5faa6307bcea4ab27d398feb64916a0af721364
Gago <tidushack@gmail.com>
2020-05-20 21:56:24 +0200

    Form
    Actualizado el formulario con los datos reales.

--- ECG Scanner/ECG Scanner/Main Views/Form.swift
+++ ECG Scanner/ECG Scanner/Main Views/Form.swift
@@ -10,11 +10,53 @@ import SwiftUI
 
 struct FormView: View {
     
+    
+    // Variables del model
+    // Datos relativos al paciente
+    @State private var id = ""
     @State private var name = ""
-    @State private var lastName = ""
-    @State private var hospital = ""
-    @State private var diabetic = false
+    @State private var firstSurname = ""
+    @State private var secondSurname = ""
+    
+    @State private var address = ""
+    @State private var city = ""
+    @State private var province = ""
+    @State private var postalCode = ""
+    
+    @State private var genre = ""
     @State private var age = 30
+    @State private var weight = 30
+    @State private var height = 30
+    //Indice de masa corporal (body mass index)
+    @State private var bmi = ""
+    @State private var smoker = false
+    @State private var allergy = ""
+    @State private var chronic = ""
+    @State private var medication = ""
+    @State private var hospital = ""
+    
+    
+    // Datos asociados al ECG del paciente
+    @State private var origin = ""
+    @State private var ecgModel = ""
+    //presion sanguinea (Body Pressure)
+    @State private var bp = 30
+    //temperatura corporal (body-temperature)
+    @State private var bt = 30
+    @State private var glucose = 30
+    @State private var reason = ""
+    @State private var ecgType = ""
+    @State private var heartRate = ""
+    
+    // Variables de la vista
+    private var genderOptions = ["Hombre", "Mujer"]
+    private var ageOptions = [0,1,2,3,4,5,6,7,8,9,10]
+    @State private var selectionGenre = 0
+    @State private var pickerVisibleGenre = false
+    @State private var selectionAge = 30
+    @State private var pickerVisibleAge = false
+
+
     
     var body: some View {
     
@@ -24,31 +66,108 @@ struct FormView: View {
                 
                 Section(header: Text("Datos generales")){
                     
+                    
                     // Nombre
                     TextField("Nombre", text: $name)
                     
-                    //Apellidos
-                    TextField("Apellidos", text: $lastName)
+                    //Primer apellido
+                    TextField("Primer apellido", text: $firstSurname)
                     
-                    // Hospital
-                    Picker(selection: $hospital, label: Text("Hospital")){
-                        ForEach(HospitalList.allHospitals, id: \.self){
-                            hospital in Text(hospital).tag(hospital)
+                    //Segundo apellido
+                    TextField("Segundo apellido", text: $secondSurname)
+                    
+                    
+                }//Datos generales
+                
+                Section(header: Text("")){
+                    
+                    //Direccion
+                    TextField("Dirección", text: $address)
+                    
+                    //Ciudad
+                    TextField("Ciudad", text: $city)
+                    
+                    //Provincia
+                    TextField("Provincia", text: $province)
+                    
+                    //PostaCode
+                    TextField("C.P", text: $postalCode)
+                    
+                    
+                    //Genero
+                    HStack{
+                        Text("Género")
+                        Spacer()
+                        Button(genderOptions[selectionGenre]){
+                            self.pickerVisibleGenre.toggle()
+                        }.foregroundColor(self.pickerVisibleGenre ? .red : .blue)
+                    }
+                    
+                    if pickerVisibleGenre {
+                        HStack{
+                            Spacer()
+                            Picker(selection: $selectionGenre, label: Text("")) {
+                                ForEach(0..<genderOptions.count) {
+                                    Text(self.genderOptions[$0]).foregroundColor(.secondary)
+                                }
+        
+                            }.pickerStyle(WheelPickerStyle())
+                                .onTapGesture {
+                                    self.pickerVisibleGenre.toggle()
+                                    self.genre = self.genderOptions[self.selectionGenre]
+                            }
+                            Spacer()
+                        }
+                    }
+                    
+                    //Edad
+                    HStack{
+                        Text("Edad")
+                        Spacer()
+                        Button("\(self.selectionAge )"){
+                            self.pickerVisibleAge.toggle()
+                        }.foregroundColor(self.pickerVisibleAge ? .red : .blue)
+                    }
+                    
+                    if pickerVisibleAge {
+                        HStack{
+                            Spacer()
+                            Picker(selection: $selectionAge, label: Text("")) {
+                                ForEach((1...120), id: \.self) {
+                                    Text("\($0)").foregroundColor(.secondary)
+                                }
+                            }
+                            .pickerStyle(WheelPickerStyle())
+                            .onTapGesture {
+                                self.pickerVisibleAge.toggle()
+                                self.age = self.selectionAge
+                                    
+                            }
+                            Spacer()
                         }
                     }
                     
                 }
+                   
+                    
+
+                    
+                
+                
+                
+                
+                
+                
+                
                 
                 Section(header: Text("Datos médicos")){
                     
                     // Diabético
-                    Toggle(isOn: $diabetic, label: {
+                    Toggle(isOn: $smoker, label: {
                         Text("Diabético")
                     })
                     
-                    Stepper(value: $age, in: 0...120, label: {
-                        Text("Edad: \(self.age) años")
-                    })
+                    
                 }
                 
             }
