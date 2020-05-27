7e99b6ba1706567c8fee6b8f28e83ce711511587
Gago <tidushack@gmail.com>
2020-05-27 22:27:13 +0200

    BMI
    Se añade el cálculo automático de BMI. Además de estilos propios del label para mostrar información de la interpretación de dicho dato.

--- ECG Scanner/ECG Scanner/Main Views/Form.swift
+++ ECG Scanner/ECG Scanner/Main Views/Form.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FormView: View {
     
     
-    // Variables del model
+    // Variables del model: Esto se pasara a un modelo Paciente
     // Datos relativos al paciente
     @State private var id = ""
     @State private var name = ""
@@ -25,10 +25,10 @@ struct FormView: View {
     
     @State private var genre = ""
     @State private var age = 30
-    @State private var weight = 50.0
-    @State private var height = 30
+    @State private var weight = 60.0
+    @State private var height = 168
     //Indice de masa corporal (body mass index)
-    @State private var bmi = ""
+    @State private var bmi = 21.25
     @State private var smoker = false
     @State private var allergy = ""
     @State private var chronic = ""
@@ -50,7 +50,6 @@ struct FormView: View {
     
     // Variables de la vista
     
-    
     @State private var selectionProvince = 0
     @State private var pickerVisibleProvince = false
     
@@ -65,6 +64,9 @@ struct FormView: View {
     
     @State private var selectionHeight = 168
     @State private var pickerVisibleHeight = false
+    
+    @State private var bmiText = "(Normal)"
+    @State private var bmiColor = Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
 
     func weightData() -> [Double]{
         
@@ -84,10 +86,83 @@ struct FormView: View {
         return String(format:"%.1f", weight_)
     }
     
-    func sanitanizeAge(weight_: Int) -> String{
+   
+    //TODO: Sacar a una interfaz
+    func getAdultCategoryBMI(bmi_: Double) -> String{
         
-        //var weightString = String(format:"%.1f", weight_)
-        return String(format:"%.1f", weight_)
+        if (bmi_ < 18.50){
+            if (bmi_ < 16.0){
+                return "(Delgadez severa)"
+            }
+            
+            if (bmi_ >= 16.0 && bmi_ <= 16.99){
+                return "(Delgadez moderada)"
+            }
+              
+            if (bmi_ >= 17.00 && bmi_ <= 18.49){
+                return "(Delgadez leve)"
+            }
+        }
+        
+        if (bmi_ >= 18.5 && bmi_ <= 24.99){
+            return "(Normal)"
+        }
+        
+        if (bmi_ >= 25.00 && bmi_ <= 29.99){
+            return "(Sobrepeso)"
+        }
+        
+        if (bmi_ >= 30.00 && bmi_ <= 34.99){
+            return "(Obesidad leve)"
+        }
+        
+        if (bmi_ >= 35.00 && bmi_ <= 39.99){
+            return "(Obesidad media)"
+        }
+        
+        if (bmi_ >= 40.00){
+            return "(Obesidad mórbida)"
+        }
+        
+        return ""
+    }
+    func getAdultColorBMI(bmi_: Double) -> Color{
+        
+        if (bmi_ < 18.50){
+            if (bmi_ < 16.0){
+                return Color(.red)
+            }
+            
+            if (bmi_ >= 16.0 && bmi_ <= 16.99){
+                return Color(.red)
+            }
+              
+            if (bmi_ >= 17.00 && bmi_ <= 18.49){
+                return Color(.orange)
+            }
+        }
+        
+        if (bmi_ >= 18.5 && bmi_ <= 24.99){
+            return Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+        }
+        
+        if (bmi_ >= 25.00 && bmi_ <= 29.99){
+            return Color(.orange)
+        }
+        
+        if (bmi_ >= 30.00 && bmi_ <= 34.99){
+            return Color(.red)
+        }
+        
+        if (bmi_ >= 35.00 && bmi_ <= 39.99){
+            return Color(.red)
+        }
+        
+        if (bmi_ >= 40.00){
+            return Color(.red)
+        }
+        
+        return Color(.black)
     }
     
     var body: some View {
@@ -104,13 +179,13 @@ struct FormView: View {
                     TextField("Nombre", text: $name).keyboardType(.numberPad)
                     
                     //Primer apellido
-                    TextField("Primer apellido", text: $firstSurname)
+                    TextField("Primer apellido", text: $firstSurname).keyboardType(.default)
                     
                     //Segundo apellido
                     TextField("Segundo apellido", text: $secondSurname)
                     
                     //Direccion
-                   TextField("Dirección", text: $address)
+                    TextField("Dirección", text: $address)
                    
                    //Ciudad
                    TextField("Ciudad", text: $city)
@@ -133,8 +208,10 @@ struct FormView: View {
                                 }
         
                             }.pickerStyle(WheelPickerStyle())
-                                .onTapGesture {
+                            .onTapGesture {
                                     self.pickerVisibleProvince.toggle()
+                                    self.province = ConstantsViews.provinceOptions[self.selectionProvince]
+                            }.onDisappear{
                                     self.province = ConstantsViews.provinceOptions[self.selectionProvince]
                             }
                             Spacer()
@@ -166,8 +243,10 @@ struct FormView: View {
                                 }
         
                             }.pickerStyle(WheelPickerStyle())
-                                .onTapGesture {
+                            .onTapGesture {
                                     self.pickerVisibleGenre.toggle()
+                                    self.genre = ConstantsViews.genderOptions[self.selectionGenre]
+                            }.onDisappear{
                                     self.genre = ConstantsViews.genderOptions[self.selectionGenre]
                             }
                             Spacer()
@@ -196,6 +275,8 @@ struct FormView: View {
                                 self.pickerVisibleAge.toggle()
                                 self.age = self.selectionAge
                                     
+                            }.onDisappear{
+                                self.age = self.selectionAge
                             }
                             Spacer()
                         }
@@ -226,7 +307,21 @@ struct FormView: View {
                             .onTapGesture {
                                 self.pickerVisibleWeight.toggle()
                                 self.weight = self.weightData()[self.selectionWeight]
-
+                                
+                                // Una vez introducido el peso debemos recalcular el BMI
+                                self.bmi = Double(self.weight) / pow((Double(self.height)/100),2)
+                                self.bmiText = self.getAdultCategoryBMI(bmi_: self.bmi)
+                                self.bmiColor = self.getAdultColorBMI(bmi_: self.bmi)
+                                
+                            }.onDisappear{
+                                
+                                self.weight = self.weightData()[self.selectionWeight]
+                                
+                                // Una vez introducido el peso debemos recalcular el BMI
+                                self.bmi = Double(self.weight) / pow((Double(self.height)/100),2)
+                                self.bmiText = self.getAdultCategoryBMI(bmi_: self.bmi)
+                                self.bmiColor = self.getAdultColorBMI(bmi_: self.bmi)
+                                
                             }
                             Spacer()
                         }
@@ -252,12 +347,38 @@ struct FormView: View {
                             .pickerStyle(WheelPickerStyle())
                             .onTapGesture {
                                 self.pickerVisibleHeight.toggle()
-                                self.age = self.selectionHeight
+                                self.height = self.selectionHeight
+                                
+                                // Una vez introducida la altura debemos recalcular el BMI
+                                self.bmi = Double(self.weight) / pow((Double(self.height)/100),2)
+                                self.bmiText = self.getAdultCategoryBMI(bmi_: self.bmi)
+                                self.bmiColor = self.getAdultColorBMI(bmi_: self.bmi)
 
                             }
+                            .onDisappear{
+                                self.height = self.selectionHeight
+                                
+                                // Una vez introducida la altura debemos recalcular el BMI
+                                self.bmi = Double(self.weight) / pow((Double(self.height)/100),2)
+                                self.bmiText = self.getAdultCategoryBMI(bmi_: self.bmi)
+                                self.bmiColor = self.getAdultColorBMI(bmi_: self.bmi)
+                            }
+                            
                             Spacer()
                         }
                     }//End Altura
+                    
+                    
+                    //BMI
+                    HStack{
+                        Text("IMC " + self.bmiText).foregroundColor(bmiColor)
+                        Spacer()
+                        Button("\(String(format:"%.1f", self.bmi )) kg/m" + "\u{00B2}")
+                        {}.foregroundColor(.gray)
+                    }
+                    
+                    
+                    
                     
                 }
                         
